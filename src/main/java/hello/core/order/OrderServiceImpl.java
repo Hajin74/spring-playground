eebc2d689d994a29e3f3e5b66296f357a37238c6
Hajin74 <yoonmij9974@naver.com>
2024-01-29 16:13:07 +0900

    인터페이스만 의존하게 수정 후 NullPointerException 발생

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,8 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -10,15 +8,13 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    // 할인 정책을 바꿀때 클라이언트인 OrderService 구현체도 변경됨 -> OCP 위반
-    // 인터페이스도 의존하고 구현체도 의존함 -> DIP 위반
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // 구현체 의존을 끊어내고, 인터페이스만 의존하게 수정 -> DIP는 지키지만 NullPointerException 발생
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
-        int discountPrice = discountPolicy.discount(member, itemPrice);
+        int discountPrice = discountPolicy.discount(member,  itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
