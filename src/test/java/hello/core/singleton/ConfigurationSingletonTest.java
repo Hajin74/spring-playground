eab13b5268fd6abafba65325b5feb41d05b53aae
Hajin74 <yoonmij9974@naver.com>
2024-01-31 15:31:01 +0900

    @configuration과 바이트코드 조작

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -6,6 +6,7 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ConfigurationSingletonTest {
@@ -27,5 +28,17 @@ public class ConfigurationSingletonTest {
 
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+        // AppConfig$$SpringCGLIB는 appConfig의 자식 클래스라 조회가 가능
+
+        System.out.println("bean = " + bean.getClass());
+        // bean = class hello.core.AppConfig$$SpringCGLIB$$0
+        // 내가 만든 클래스가 아니라 스프링이 바이트코드 조작 라이브러리를 사용해 다른 클래스를 만들어 스프링 빈으로 등록!
+        // 이 임의의 다른 클래스가 싱글톤이 보장되도록 해줌
     }
 }
