7e2994adf5464b921137e07cd61ecfe728805a22
Hajin74 <yoonmij9974@naver.com>
2024-02-09 11:40:50 +0900

    Spring MVC 시작

---
+++ src/main/java/practice/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,14 @@
+package practice.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller // @Component @RequestMapping or 직접 @Bean 으로 등록해도 됨
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
