0765f4f6ff93206df7dd0a1c949cf7aac812bd39
Hajin74 <yoonmij9974@naver.com>
2024-02-10 22:25:43 +0900

    git test

--- Spring-MVC-1/src/main/java/practice/springmvc/SpringmvcApplication.java
+++ Spring-MVC-1/src/main/java/practice/springmvc/SpringmvcApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringmvcApplication {
-
+	// git test
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcApplication.class, args);
 	}
