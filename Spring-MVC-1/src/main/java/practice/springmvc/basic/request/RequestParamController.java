e2763577b8fc412a15f1690b7140965619255b5c
Hajin74 <yoonmij9974@naver.com>
2024-02-13 12:26:07 +0900

    HTTP 요청 파라미터 - @RequestParam

--- Spring-MVC-1/src/main/java/practice/springmvc/basic/request/RequestParamController.java
+++ Spring-MVC-1/src/main/java/practice/springmvc/basic/request/RequestParamController.java
@@ -5,8 +5,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -20,4 +23,54 @@ public class RequestParamController {
 
         response.getWriter().write("ok");
     }
+
+    @ResponseBody // 반환값을 뷰로 주는게 아니라 메시지 바디에 문자로 콱 박아버림
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam("username") String memberName,
+                                 @RequestParam("age") int memberAge) {
+        log.info("username = {}, age = {}", memberName, memberAge);
+        return "ok2";
+    }
+
+    @ResponseBody // 반환값을 뷰로 주는게 아니라 메시지 바디에 문자로 콱 박아버림
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(@RequestParam String username,
+                                 @RequestParam int age) {
+        log.info("username = {}, age = {}", username, age);
+        return "ok3";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username = {}, age = {}", username, age);
+        return "ok4";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(@RequestParam(required = true) String username,
+                                       @RequestParam(required = false) Integer age) {
+        // int 기본형 타입에는 null이 들어갈 수가 없음. 객체인 Integer를 써야함.
+        // null과 ""는 달라 -> ""는 빈 문자라는 값으로 인식
+        log.info("username = {}, age = {}", username, age);
+        return "ok required";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(@RequestParam(required = true, defaultValue = "guest") String username,
+                                      @RequestParam(required = false, defaultValue = "-1") Integer age) {
+        // 빈 문자에 경우에도 디폴트값으로 대체해 처리해버림
+        log.info("username = {}, age = {}", username, age);
+        return "ok default";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username = {}, age = {}", paramMap.get("username"), paramMap.get("age"));
+        return "ok map";
+    }
+
 }
