f2ee83966296d6415f246a1aca1f8187d884758e
Hajin74 <yoonmij9974@naver.com>
2024-03-10 03:43:43 +0900

    스프링 시큐리티

---
+++ SpringJWT/src/main/java/org/example/springjwt/service/JoinService.java
@@ -0,0 +1,35 @@
+package org.example.springjwt.service;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.example.springjwt.dto.JoinDTO;
+import org.example.springjwt.entity.UserEntity;
+import org.example.springjwt.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JoinService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void join(JoinDTO joinDTO) {
+        String username = joinDTO.getUsername();
+        String password = joinDTO.getPassword();
+
+        Boolean isExist = userRepository.existsByUsername(username);
+        if (isExist) {
+            return ;
+        }
+
+        UserEntity data = new UserEntity();
+        data.setUsername(username);
+        data.setPassword(bCryptPasswordEncoder.encode(password));
+        data.setRole("ROLE_ADMIN");
+
+        userRepository.save(data);
+    }
+
+}
