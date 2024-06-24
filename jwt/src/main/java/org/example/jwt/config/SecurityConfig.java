ed0bbc251b42671e1151e9ae0585f8e965927c99
Hajin <yoonmij9974@naver.com>
2024-06-24 14:09:03 +0900

    feat: 로그아웃

--- jwt/src/main/java/org/example/jwt/config/SecurityConfig.java
+++ jwt/src/main/java/org/example/jwt/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.example.jwt.config;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.example.jwt.jwt.CustomLogoutFilter;
 import org.example.jwt.jwt.JWTFilter;
 import org.example.jwt.jwt.JWTUtil;
 import org.example.jwt.jwt.LoginFilter;
@@ -15,6 +16,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -90,6 +92,9 @@ public class SecurityConfig {
 
         http
                 .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, refreshRepository), UsernamePasswordAuthenticationFilter.class);
+
+        http
+                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class);
 
         //세션 설정
         http
